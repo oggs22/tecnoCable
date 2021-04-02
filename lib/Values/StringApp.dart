4b24cf43c71d3c760a9aa795d76c6eb302523bc8
Oriana Graterol <oggs22@gmail.com>
2021-04-02 18:23:56 -0400

    added header strings

--- lib/Values/StringApp.dart
+++ lib/Values/StringApp.dart
@@ -1,1 +1,8 @@
 //String Menu
+
+//String Drawer y footer
+String get aboutUsStr => "¿Quiénes somos?";
+String get contactUsStr => "Contáctanos";
+String get servicesStr => "Servicios";
+String get knowUsStr => "Técnicos";
+String get ratesStr => "Tarifas";
