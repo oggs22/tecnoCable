8f2887ced786e91f0a714cdb888b73153fa8be62
Oriana Graterol <oggs22@gmail.com>
2021-04-02 21:06:18 -0400

    added new background pink

--- lib/Values/ColorsApp.dart
+++ lib/Values/ColorsApp.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const backgroundWhiteColor = const Color(0xffF5F5F5);
 
 const backgroundBlueColor = const Color(0xff66A3FF);
+
+const backgroundPinkColor = const Color(0xffEC008C);
