9ccae499c4be19f39769558f5699b5ba7bb64570
Oriana Graterol <oggs22@gmail.com>
2021-04-02 22:15:31 -0400

    add bar white color

--- lib/Values/ColorsApp.dart
+++ lib/Values/ColorsApp.dart
@@ -5,3 +5,5 @@ const backgroundWhiteColor = const Color(0xffF5F5F5);
 const backgroundBlueColor = const Color(0xff66A3FF);
 
 const backgroundPinkColor = const Color(0xffEC008C);
+
+const barWhiteTextColor = const Color(0xffF6F6F6);
