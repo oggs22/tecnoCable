13fe85c9a1a7dc95921032f1ebd50331cf6f4717
Oriana Graterol <oggs22@gmail.com>
2021-04-19 19:17:13 -0400

    fixed card technical visit

--- lib/Widgets/WebComponents/Body/Section/TechnicalVisitSection.dart
+++ lib/Widgets/WebComponents/Body/Section/TechnicalVisitSection.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tecnocable/Values/ResponsiveApp.dart';
 import 'package:tecnocable/Values/StringApp.dart';
-import 'package:tecnocable/Widgets/WebComponents/Body/Container/TechnicalVisitListView.dart';
+import 'package:tecnocable/Widgets/WebComponents/Body/Container/TechnicalVisitContainer.dart';
 import 'package:tecnocable/Widgets/Components/TitleWithTwoColors.dart';
 import 'package:tecnocable/Models/TechnicalVisit.dart';
 
 class TechnicalVisitSection extends StatelessWidget {
   TechnicalVisitSection();
   ResponsiveApp responsiveApp;
+  List<TechnicalVisit> list;
 
   var _container = Container(
     height: 200,
@@ -34,52 +35,15 @@ class TechnicalVisitSection extends StatelessWidget {
                   ),
                   SizedBox(width: responsiveApp.aboutUsButtonSpace),
                   Container(
-                      height: 200,
+                      height: 400,
                       width: 710,
                       child: SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Row(
                           children: [
-                            Container(
-                              width: 160.0,
-                              color: Colors.red,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.blue,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.green,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.yellow,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.orange,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.red,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.blue,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.green,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.yellow,
-                            ),
-                            Container(
-                              width: 160.0,
-                              color: Colors.orange,
-                            ),
+                            TechnicalVisitContainer(technicalVisitList[0]),
+                            TechnicalVisitContainer(technicalVisitList[1]),
+                            TechnicalVisitContainer(technicalVisitList[2]),
                           ],
                         ),
                       )),
